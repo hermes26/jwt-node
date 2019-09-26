208a5eda5661623b7d2e1adb7c8870f376296d0f
hermes26 <hermitte.thierry@gmail.com>
2019-09-26 02:03:18 +0200

    modelo user y encryption del password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,22 @@
 const { Router } = require('express');
 const router = Router();
 
-router.post('/login', (req, res, next) => {
-    res.json('login')
+const User = require('../models/User')
+
+router.post('/login', async (req, res, next) => {
+    const { username, email, password } = req.body;
+   // User.create({ //user.create() es un metodo del modelo para guardar un dato directamente en la bd
+    const user = new User({ //crea un objeto que despues voy a guardar en mi bd. ese obj tendra un id anadido tambien
+        username,
+        email,
+        password
+    })
+    user.password = await user.encryptPassword(user.password);
+    await user.save();
+    console.log(user);
+    res.json({message: 'received'});
+        
+    //})
 })
 
 router.post('/register', (req, res, next) => {
--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,16 @@
 const { Schema, model } = require('mongoose'); //ahora no va importar mongoose para connectar sino algunos metodos que tiene
+const bcrypt = require('bcryptjs');
+
 
 const userSchema = new Schema({//el schema es que data voy a estar guardando. DESCRIBIENDO
     username: String,
     email: String,
     password: String,
-})
+});
+
+userSchema.methods.encryptPassword = async (password) => { //este metodo encryptPassword ahora apartenece al modelo User
+    const salt = await bcrypt.genSalt(10);
+    return bcrypt.hash(password, salt);
+}
 
 module.exports = model('User', userSchema);//para crear en la bd. en la bd voy a guardar un modelo de usuario que voy a llamar 'User', y va a estar basado en el userSchema 
