7c6e951d2b22ce1f6db0402359d617c0b68c2c66
hermes26 <hermitte.thierry@gmail.com>
2019-09-26 02:33:18 +0200

    secret and token

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    secret: 'mysecret'
+}
+
+//mucho mas recomendable guardarlo en un variable de entorno porq tiene que ser privado
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,27 @@
 const { Router } = require('express');
 const router = Router();
 
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
 const User = require('../models/User')
 
 router.post('/login', async (req, res, next) => {
     const { username, email, password } = req.body;
    // User.create({ //user.create() es un metodo del modelo para guardar un dato directamente en la bd
-    const user = new User({ //crea un objeto que despues voy a guardar en mi bd. ese obj tendra un id anadido tambien
+    const user = new User({ //crea un objeto que despues voy a guardar en mi bd. ese obj tendra un _id anadido tambien
         username,
         email,
         password
     })
     user.password = await user.encryptPassword(user.password);
     await user.save();
-    console.log(user);
-    res.json({message: 'received'});
+
+    const token = jwt.sign({id: user._id}, config.secret, {//sign-permite registrar/crear un token
+        expiresIn: 60 * 60 * 24//3 param - cuanto tiempo va a durar el token
+    }); 
+    
+    res.json({auth: true, token: token});//va a pasar al usuario el mensaje auth:true y el token
         
     //})
 })
