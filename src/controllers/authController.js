685bb7804b3b9434f7056ae488bc6b345fd2b729
hermes26 <hermitte.thierry@gmail.com>
2019-09-27 14:23:55 +0200

    profile route with authentication of token in header

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,7 @@ const User = require('../models/User')
 
 router.post('/login', async (req, res, next) => {
     const { username, email, password } = req.body;
-   // User.create({ //user.create() es un metodo del modelo para guardar un dato directamente en la bd
+   // User.create({}) //user.create() es un metodo del modelo para guardar un dato directamente en la bd
     const user = new User({ //crea un objeto que despues voy a guardar en mi bd. ese obj tendra un _id anadido tambien
         username,
         email,
@@ -20,18 +20,35 @@ router.post('/login', async (req, res, next) => {
     const token = jwt.sign({id: user._id}, config.secret, {//sign-permite registrar/crear un token
         expiresIn: 60 * 60 * 24//3 param - cuanto tiempo va a durar el token
     }); 
-    
+
     res.json({auth: true, token: token});//va a pasar al usuario el mensaje auth:true y el token
         
-    //})
+})
+
+router.get('/profile', async (req, res, next) => { //usuario puede acceder solamente si tiene authorisacion. asi que primero hay que ver si el usuario tiene un token o no
+
+    //cuando enviamos un token, existe una cabezera/header x-access-token con valor el token que tenemos
+    //en el servidor comprobamos que existe esa header, entonces tiene un token, y entonces authorisacion para hacer algo
+    const token = req.headers['x-access-token'];
+    if(!token){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        })
+    };
+
+    const decoded = jwt.verify(token, config.secret);
+    const user = await User.findById(decoded.id, { password: 0 }); //si no quieres devolver un dato en mongoose eg password: 0
+    if(!user){
+        return res.status(404).send('No user found');
+    }
+
+    res.json(user);
 })
 
 router.post('/register', (req, res, next) => {
     res.json('register')
 })
 
-router.get('/profile', (req, res, next) => {
-    res.json('profile')
-})
 
 module.exports = router;
